764e696f163374209a4d08e5216e39607f543d76
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-04-28 22:45:38 +0530

    added folders

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express=  require("express")
+const cors = require('cors')
+const mongoose = require('mongoose') // helps us to connect to mongo db
+
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI // data base usi-> we get from mongodb dash
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true}) // we connect to uri here
+
+app.listen(5000, ()=>{
+    console.log("ON PORT", 5000)
+})
